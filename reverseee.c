62237b3e6c507ea230fa7bcdb6fe4b504e8027bd
jeevana-049 <jeevanareddy004@gmail.com>
2025-02-11 16:05:05 +0530

    Add files via upload

---
+++ reverseee.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int n,rem;
+    int rev=0;
+	printf("Enter a number : ");
+	scanf("%d",&n);
+	while(n!=0)
+	{
+		rem=n%10;
+		n=n/10;
+		rev=rev*10+rem;
+	}
+	printf("%d",rev);
+}
